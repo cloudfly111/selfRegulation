4ef0c988fed61c2f509ab8dca760f668dc9f86ab
Chan Shiang-Yun <cloudfly4326@gmail.com>
2024-01-09 10:20:25 +0800

    首頁： 1.建立狀態排序範例資料 2.新增狀態排序標題

--- app/src/main/java/com/judy/self/regulation/fragment/MainFragment.kt
+++ app/src/main/java/com/judy/self/regulation/fragment/MainFragment.kt
@@ -36,15 +36,28 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        val mapData = mutableMapOf<String,String>()
-        mapData.put("RANK","01")
-        mapData.put("TITLE","健康")
-        val list = mutableListOf(mapData,mapData,mapData)
+
+        val list = setListData()
         val adapter = MainScreenAdapter(list)
         adapter.notifyDataSetChanged()
-        binding.rankList.layoutManager = LinearLayoutManager(context)
-        binding.rankList.adapter=adapter
+        binding.rankList.apply {
+            this.layoutManager = LinearLayoutManager(context)
+            this.adapter = adapter
+        }
+    }
 
+    fun setListData():MutableList<MutableMap<String,String>>{
+        val RANK_KEY:String = "RANK"
+        val TITLE_Key:String = "TITLE"
+        val rankArray= resources.getStringArray(R.array.rankArray)
+        val categoryArray= resources.getStringArray(R.array.categoryArray)
+        val list = MutableList<MutableMap<String,String>>(3){index: Int ->
+            val mapData = mutableMapOf<String,String>()
+            mapData.put(RANK_KEY, rankArray.get(index).toString())
+            mapData.put(TITLE_Key,categoryArray.get(index).toString())
+            mapData
+        }
+        return list
     }
 
 }
